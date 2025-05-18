6245910f6473f0b5bd29ee8b4311c2b3cd89331d
Rodion Azaronak <rodionazaronak@gmail.com>
2025-05-18 13:59:13 +0300

    Add task03

--- StudentCppRevisionTask06Project/task03.cpp
+++ StudentCppRevisionTask06Project/task03.cpp
@@ -43,5 +43,9 @@
 */
 
 bool task03(int x1, int y1, int x2, int y2) {	
-	return false;
+	return x1 == x2 && y2 - y1 == 1 && (y2 == 8 || y2 == 1)
+		&& x1 > 0 && x1 <= 8
+		&& x2 > 0 && x2 <= 8
+		&& y1 > 1 && y1 <= 8
+		&& y2 > 1 && y2 <= 8;;
 }
